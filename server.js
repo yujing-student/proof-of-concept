fbadada646b796add601fc13b3ff664e37a4f2eb
yujing <medischdossier03@gmail.com>
2024-06-03 20:28:45 +0200

    refactor:onnodige code weggehaald

---
+++ server.js
@@ -0,0 +1,57 @@
+// Importeerd npm pakket express uit de node_modules map
+import express from 'express'
+// Importeerd de zelfgemaakte functie fetchJson uit de ./helpers map
+import fetchJson from './helpers/fetch-json.js'
+
+const app = express()
+
+// Stel ejs in als template engine
+app.set('view engine', 'ejs')
+// Stel de map met ejs templates in
+app.set('views', './views')
+
+// Gebruik de map 'public' voor statische resources
+app.use(express.static('public'))
+
+// Zorg dat werken met request data makkelijker wordt
+app.use(express.urlencoded({extended: true}))
+
+
+
+
+app.get('/', (request, response) => {
+    fetchJson('https://api.mobile.bnr.nl/v1/articles')
+
+
+        .then(articles => {
+            console.log('Fetched articles:', articles);
+            // http://25683.live.streamtheworld.com/BNR_BUSINESS_BEATS.mp3   link naar audio
+            articles.forEach(article => {
+                console.log(`Article ID: ${article.id}`); // Assuming 'id' is a property
+                console.log(`Article Title: ${article.title}`);  // Assuming 'title' is a property
+                // Access other properties as needed
+            });
+            const audioUrl = 'http://25683.live.streamtheworld.com/BNR_BUSINESS_BEATS.mp3';
+
+            response.render('index', {
+                articles,
+                audioUrl,
+            });
+        })
+        .catch(error => {
+            console.error('Failed to fetch articles:', error);
+            // Handle error case (e.g., display an error message in the template)
+            return response.render('error', { message: 'Failed to fetch articles' });
+        });
+})
+
+
+// Stel het poortnummer in waar express op moet gaan luisteren
+app.set('port', process.env.PORT || 8001)
+
+// Start express op, haal daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get('port'), function () {
+    // Toon een bericht in de console en geef het poortnummer door
+    console.log(`Application started on http://localhost:${app.get('port')}`)
+})
+
