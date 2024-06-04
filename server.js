5484157ce74f09b5d3d7c85a4e772c1af129f0f8
yujing <medischdossier03@gmail.com>
2024-06-04 13:56:07 +0200

    build:slice proberen toe te voegen

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import fetchJson from './helpers/fetch-json.js'
 
 const app = express()
 
+const audioUrl = 'http://25683.live.streamtheworld.com/BNR_BUSINESS_BEATS.mp3';
 // Stel ejs in als template engine
 app.set('view engine', 'ejs')
 // Stel de map met ejs templates in
@@ -16,33 +17,26 @@ app.use(express.static('public'))
 // Zorg dat werken met request data makkelijker wordt
 app.use(express.urlencoded({extended: true}))
 
-
-
-
 app.get('/', (request, response) => {
     fetchJson('https://api.mobile.bnr.nl/v1/articles')
-
-
         .then(articles => {
             console.log('Fetched articles:', articles);
             // http://25683.live.streamtheworld.com/BNR_BUSINESS_BEATS.mp3   link naar audio
             articles.forEach(article => {
-                console.log(`Article ID: ${article.id}`); // Assuming 'id' is a property
-                console.log(`Article Title: ${article.title}`);  // Assuming 'title' is a property
+                // console.log(`Article ID: ${article.id}`); // Assuming 'id' is a property
+                // console.log(`Article Title: ${article.title}`);  // Assuming 'title' is a property
+                const firstTenArticles = article.slice(0, 10);
                 // Access other properties as needed
             });
-            const audioUrl = 'http://25683.live.streamtheworld.com/BNR_BUSINESS_BEATS.mp3';
+
+
+
 
             response.render('index', {
-                articles,
+                firstTenArticles,
                 audioUrl,
             });
         })
-        .catch(error => {
-            console.error('Failed to fetch articles:', error);
-            // Handle error case (e.g., display an error message in the template)
-            return response.render('error', { message: 'Failed to fetch articles' });
-        });
 })
 
 
