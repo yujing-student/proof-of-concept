0733a03e24a2c5f1f5d7aeaa214325b81631bf65
Annevd <144004647+Annevd@users.noreply.github.com>
2024-06-04 10:05:07 +0200

    V1 Server side opzet

--- server.js
+++ server.js
@@ -1,1 +1,50 @@
-console.log('Hier komt je server voor Sprint 11.')
+/*** Express setup & start ***/
+
+// 1. Opzetten van de webserver
+
+// Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
+import fetchJson from "./helpers/fetch-json.js";
+
+// Importeer het npm pakket express uit de node_modules map
+import express from "express";
+
+// Stel het basis endpoint in
+const apiUrl = "https://fdnd-agency.directus.app/items";
+
+// Maak een nieuwe express app aan
+const app = express();
+
+// Stel ejs in als template engine
+// View engine zorgt ervoor dat data die je ophaalt uit de api , waar je in je code dingen mee doet, daar html van maakt
+app.set("view engine", "ejs");
+
+// Stel de map met ejs templates in
+app.set("views", "./views");
+
+// Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
+app.use(express.static("public"));
+
+// Zorg dat werken met request data makkelijker wordt
+app.use(express.urlencoded({ extended: true }));
+
+/*** Routes & data ***/
+
+// 2. Routes die HTTP Request and Responses afhandelen
+
+// Maak een GET route voor de index
+
+app.get("/", function (request, response) {
+  response.render("index");
+});
+
+// 3. Start de webserver
+
+// Stel het poortnummer in waar express op moet gaan luisteren
+app.set("port", process.env.PORT || 8000);
+
+// Start express op, haal daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get("port"), function () {
+  // Toon een bericht in de console en geef het poortnummer door
+  console.log(`Application started on http://localhost:${app.get("port")}`);
+});
+
