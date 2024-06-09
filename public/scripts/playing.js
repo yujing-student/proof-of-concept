08ed67abd8b13acef4c1d8e422e84cba0713136e
yujing <medischdossier03@gmail.com>
2024-06-09 16:37:38 +0200

    build:player gemaakt en die werkt ook

---
+++ public/scripts/playing.js
@@ -0,0 +1,37 @@
+ playing()
+hamburger()
+
+function playing() {
+  // Get the video
+    const audio = document.querySelector('.audio'); // Use querySelector for efficiency
+    const playButton = document.querySelector('.pauzebutton');
+
+    function togglePlayPause() {
+        if (audio.paused) {
+            console.log('video is playing')
+            audio.play();
+            playButton.classList.add('pauzebutton-open');
+            playButton.classList.remove(playButton);
+            playButton.classList.remove('pauzebutton');
+        } else {
+            audio.pause();
+            playButton.classList.remove('pauzebutton-open');
+        }
+    }
+
+    playButton.addEventListener('click', togglePlayPause);
+}
+
+function hamburger (){
+    let eersteStreepjesButton = document.querySelector("section.streepjes li:nth-of-type(1) button");
+    eersteStreepjesButton.onclick = eersteStreepjesButtonKlik;
+    function eersteStreepjesButtonKlik() {
+        if (eersteStreepjesButton.ariaExpanded == "false") {
+            eersteStreepjesButton.ariaExpanded = "true"
+        }
+        else {
+            eersteStreepjesButton.ariaExpanded = "false"
+        }
+    }
+    eersteStreepjesButtonKlik();
+}
