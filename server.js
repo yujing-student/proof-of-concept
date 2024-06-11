d9a804ac5273307717b6f82f0a084c4094175069
yujing <medischdossier03@gmail.com>
2024-06-11 16:57:16 +0200

    build:volume staat goed er mist alleen nog styling

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ app.use(express.static('public'))
 
 // Zorg dat werken met request data makkelijker wordt
 app.use(express.urlencoded({extended: true}))
+// Stel het poortnummer in waar express op moet gaan luisteren
+app.set('port', process.env.PORT || 8004)
+
+// Start express op, haal daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get('port'), function () {
+    // Toon een bericht in de console en geef het poortnummer door
+    console.log(`Application started on http://localhost:${app.get('port')}`)
+})
+
 
 app.get('/', (request, response) => {
     fetchJson('https://api.mobile.bnr.nl/v1/articles')
@@ -30,12 +39,4 @@ app.get('/', (request, response) => {
 })
 
 
-// Stel het poortnummer in waar express op moet gaan luisteren
-app.set('port', process.env.PORT || 8004)
-
-// Start express op, haal daarbij het zojuist ingestelde poortnummer op
-app.listen(app.get('port'), function () {
-    // Toon een bericht in de console en geef het poortnummer door
-    console.log(`Application started on http://localhost:${app.get('port')}`)
-})
 
