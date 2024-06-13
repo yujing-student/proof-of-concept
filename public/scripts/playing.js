39b3850f45e2a4b9ffb2ba64b101657bd01512ba
yujing <medischdossier03@gmail.com>
2024-06-13 15:16:58 +0200

    change: refactoren code

--- public/scripts/playing.js
+++ public/scripts/playing.js
@@ -1,65 +1,55 @@
- playing()
- volume()
-function playing() {
-  // Get the video
-    const audio = document.querySelector('.audio'); // Use querySelector for efficiency
-    const playButton = document.querySelector('.playbutton');
-    const pausebutton = document.querySelector('.pauzebutton');
-    // .pauzebutton
+const audio = document.querySelector('.audio');
+const playButton = document.querySelector('.playbutton');
+const pausebutton = document.querySelector('.pauzebutton');
+const volumeInput = document.querySelector('.playlist__inputRange');
 
-    function togglePlayPause() {
-        if (audio.paused) {
-            console.log('video is playing')
-            audio.play();
+volume()
+playButton.addEventListener('click', togglePlayPause);
 
-            if (document.startViewTransition) {
-                document.startViewTransition(function () {
-                    playButton.classList.remove('grow')
-                    pausebutton.classList.add('pauzebutton-open');
-                    playButton.classList.remove(playButton);
-                    playButton.classList.remove('pauzebutton');
-                });
-            }
-            else {
+
+function togglePlayPause() {
+    if (audio.paused) {
+        console.log('video is playing')
+        audio.play();
+
+        if (document.startViewTransition) {
+            document.startViewTransition(function () {
                 playButton.classList.remove('grow')
                 pausebutton.classList.add('pauzebutton-open');
-                playButton.classList.remove(playButton);
                 playButton.classList.remove('pauzebutton');
-            }
+            });
+        } else {
+            playButton.classList.remove('grow')
+            pausebutton.classList.add('pauzebutton-open');
+            playButton.classList.remove('pauzebutton');
+        }
 
 
-        }
-        else {
-            audio.pause();
-            playButton.classList.add('grow')
-            pausebutton.classList.remove('pauzebutton-open');
-        }
+    } else {
+        audio.pause();
+        playButton.classList.add('grow')
+        pausebutton.classList.remove('pauzebutton-open');
     }
-
-    playButton.addEventListener('click', togglePlayPause);
 }
 
- function volume () {
-     const volume = document.querySelector('.volume');
-     const audio = document.querySelector('.audio');
+function volume() {
 
-     volume.addEventListener('input', function() {
-         audio.volume = volume.value / 10;
-     });
- }
+    volumeInput.addEventListener('input', function () {
+        audio.volume = volumeInput.value / 10;
+    });
+}
 
 
+// deze functie werkt niet het idee is dat het pijltje aan de zijkant pas zichtbaar is als je naar beneden scrollt
+function toggleBackToTop() {
+    const backToTop = document.querySelector(".backToTop");
+    const scrollPosition = window.scrollY;
+    const halfPage = document.documentElement.clientHeight / 2;
 
- // deze functie werkt niet
- function toggleBackToTop() {
-     const backToTop = document.querySelector(".backToTop");
-     const scrollPosition = window.scrollY;
-     const halfPage = document.documentElement.clientHeight / 2;
-
-     if (scrollPosition >= halfPage) {
-         backToTop.classList.add("backToTop-open");
-         backToTop.classList.remove("backToTop");
-     }
- }
+    if (scrollPosition >= halfPage) {
+        backToTop.classList.add("backToTop-open");
+        backToTop.classList.remove("backToTop");
+    }
+}
 
 
